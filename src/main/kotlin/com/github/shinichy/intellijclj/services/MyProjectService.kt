cf4bc04f71a14c8b2f8977d6ff48bd5a352ddb07
GitHub Action <action@github.com>
2021-12-30 06:34:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/shinichy/intellijclj/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shinichy/intellijclj/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shinichy.intellijclj.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shinichy.intellijclj.MyBundle
 
 class MyProjectService(project: Project) {
 
